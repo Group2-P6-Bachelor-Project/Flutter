b835655628da4dbf432db69817b729f892a7ef38
BlackChakers <thetroleklolekpl@gmail.com>
2023-03-06 12:38:02 +0100

    First look of the Home Screen and addition of the assets folder

--- flutter_application_1/lib/firstscreen.dart
+++ flutter_application_1/lib/firstscreen.dart
@@ -5,16 +5,63 @@ class MyHomePageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Begin Recycling'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => CameraScreen()),
-            );
-          },
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("assets/chopstick.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 75),
+              height: 50,
+              width: 400,
+              color: Colors.amber,
+              child: const Text(
+                'Begin Recycling',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontStyle: FontStyle.normal,
+                  fontSize: 35,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Spacer(),
+            Container(
+              height: 50,
+              width: 400,
+              color: Colors.amber,
+              child: const Text(
+                'Begin Recycling',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontStyle: FontStyle.normal,
+                  fontSize: 16,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Spacer(),
+            Container(
+              height: 60,
+              width: 200,
+              margin: EdgeInsets.only(bottom: 50),
+              child: ElevatedButton(
+                child: const Text('Begin Recycling'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CameraScreen()),
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
