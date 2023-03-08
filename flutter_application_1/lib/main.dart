3ba98bcece63ccc0050488712469ac2ee42b1901
Mandrupnicolai <nicolai.uhre.com@gmail.com>
2023-03-08 11:10:25 +0100

    merged together with former branches

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/firstscreen.dart';
-import 'package:flutter_application_1/OutputScreen.dart';
+import 'package:flutter_application_1/firstscreen.dart';
+// import 'package:flutter_application_1/OutputScreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) => MaterialApp(home: OutputScreen());
+  Widget build(BuildContext context) =>
+      const MaterialApp(home: MyHomePageState());
 }
